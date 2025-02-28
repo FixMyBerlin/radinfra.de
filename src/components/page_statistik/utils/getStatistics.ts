1781314df6b4da925041f3fded808e2708ef1561
Tobias <tobias@fixmycity.de>
2025-02-28 07:07:14 +0100

    Cleanup

--- src/components/page_statistik/utils/getStatistics.ts
+++ src/components/page_statistik/utils/getStatistics.ts
@@ -7,7 +7,7 @@ export type CardProps = { level: Level; bundeslandId?: string | undefined }
 export const getStatistics = async ({ level, bundeslandId }: CardProps) => {
   const landkreisOsmIds = bundeslandId ? bundeslandLandkreis.get(bundeslandId) : []
   const unfilteredStatistics = await getCollection('statistics')
-  console.log('landkreisOsmIds', bundeslandId)
+
   const statistics = unfilteredStatistics
     .filter((stat) => stat.data.properties.level === levelMap.get(level))
     .filter((stat) => (bundeslandId ? landkreisOsmIds?.includes(stat.data.id) : true))
