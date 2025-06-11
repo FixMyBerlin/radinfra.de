56ba33229853be7588c69a42320cd3b0cd7e0126
Tobias <tobias@fixmycity.de>
2025-06-11 15:32:16 +0200

    Campaigns: Testing data

--- src/components/fetch/tildaApiUrl.const.ts
+++ src/components/fetch/tildaApiUrl.const.ts
@@ -1,3 +1,5 @@
 export const tildaApiUrl = 'https://tilda-geo.de/api'
+// export const tildaApiUrl = 'https://staging.tilda-geo.de/api'
+// export const tildaApiUrl = 'http://127.0.0.1:5173/api'
 
 export const tildaStatisticsApiUrl = `${tildaApiUrl}/stats`
